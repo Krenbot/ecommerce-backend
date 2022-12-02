22aac04e8864ad723527166a7cffc426f81ef84f
Steve Krenn <stevekrenn@gmail.com>
2022-12-01 18:12:52 -0600

    added get one and all for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    console.log(product);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!product) {
+      res
+        .status(400)
+        .json({ message: "Product not found. Try different ID number." });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
